a503a5e391ab23212a7aed256d0af91033ca4065
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2020-10-18 17:00:39 +0300

    feat: refactor solution

--- src/index.js
+++ src/index.js
@@ -1,105 +1,80 @@
 module.exports = function check(str, bracketsConfig) {
-  let array = transformStringToArray(str);
+  let array = str.split('');
   let arrayLength = array.length;
   let check1 = checkIfArrayLengthIsEven(array);
   // console.log(check1)
+  let stack = [];
+
   if (check1) {
-    let check2 = checkIfBracketIsOpening(array[0]);
-    let check3 = checkIfBracketIsNeutral(array[0]);
-    // console.log(check2)
-    // console.log(check3)
-    // console.log(check2 || check3)
-    if (check2 || check3) {
-      for (let i = 0; i <= arrayLength/2; i++) {
-        let index = findPairOfBrackets(array);
-        // console.log(index)
-        // console.log(array[index])
-        // console.log(array)
-        // console.log("array.length", array.length)
-        if(array[index] !== '|') {
-          if (index) {
-            array.splice(index-1, 2)
+    let checkOpening = checkIfBracketIsOpening(array[0]);    
+    let checkNeutral = checkIfBracketIsNeutral(array[0]);
+
+    if (checkOpening || checkNeutral) {
+      for (let i = 0; i < array.length; i++) {
+        let checkOpening = checkIfBracketIsOpening(array[i]);    
+        let checkNeutral = checkIfBracketIsNeutral(array[i]);
+        // console.log(array[i])
+        // console.log(checkOpening)
+        // console.log(checkNeutral)
+        if (checkNeutral) {
+          let pair = checkForOpeningPair(array[i]);
+          if(stack[stack.length-1] === pair) {
+            stack.pop()
           } else {
-            i = arrayLength
-          }          
-        } else if (array[index] === '|') {
-          array.splice(arrayLength/2+1, 1)
-          array.splice(index, 1)
+            stack.push(array[i])
+          }
+        } else if (checkOpening) {
+          stack.push(array[i])
+          // console.log(stack)
         } else {
-          i = arrayLength
-        }
+          // console.log(stack)
+          let pair = checkForOpeningPair(array[i]);
+          // console.log(pair)
+          // console.log(stack[stack.length-1] === pair)
+          if(stack[stack.length-1] === pair) {
+            stack.pop()
+          } else {
+            stack.push(false)
+            break
+          }
+        }        
       }
-      return (array.length == 0)? true:false;    
+    } else {
+      return false
+    }    
+
+    if (stack.length == 0) {
+      return true
     }
+  
+    if (stack.includes(false)) {
+      return false
+    }
+
   } else {
-    return false;
-  }
+    return false
+  }    
 }
 
-function transformStringToArray(str) {
-  let arr = str.split('');
-  return arr;
-}
-function checkIfArrayLengthIsEven (array){
+function checkIfArrayLengthIsEven (array) {
   return (array.length % 2 == 0)? true : false;
 }
-function checkIfBracketIsOpening (bracket){
-  let openingBrackets = ['(', '[', '{', '|'];
-  let result = openingBrackets.filter(br => bracket === br); 
+
+function checkIfBracketIsOpening (bracket) {
+  let openingBrackets = ['(', '[', '{'];
+  let result = openingBrackets.includes(bracket); 
   return (result)? true : false;
 }
-function findFirstClosingBracket(array) {
-  let indexOfFirstClosingBracket = array.findIndex(br => br === ']' || br === '}' || br === ')' || br === '|');
-  let check = checkIfBracketIsNeutral(array[indexOfFirstClosingBracket]);
-  if (!check) {
-    return (indexOfFirstClosingBracket)? indexOfFirstClosingBracket : false;
-  } else {
-    return indexOfFirstClosingBracket;
-  }  
-}
+
 function checkIfBracketIsNeutral(bracket) {
   let neutralBracket = '|';
-  let result = (bracket === neutralBracket)? true : false; 
-  return result;
-}
-function checkIfBracketsAreMatch(array, index) {
-  let openingBrackets = ['(', '[', '{', '|'];
-  let closingBrackets = [')', ']', '}', '|'];
-  let keyOfClosingBracket = closingBrackets.findIndex(br => br === array[index]);
-  let keyOfOpeningBracket = openingBrackets.findIndex(br => br === array[index - 1]);
-  return (keyOfClosingBracket == keyOfOpeningBracket)? true:false;
-}
-function checkForSecondNeutralBracket(array, indexOfFirstNeutralBracket) {
-  let result = checkIfBracketsAreMatch(array, indexOfFirstNeutralBracket+1); // check for || that stay close, one after another
-  if (!result) { // check for such case |()| - between even number of brackets
-    let newArray = array.slice();
-    newArray.splice(indexOfFirstNeutralBracket, 1);
-    let indexOfSecondNeutralBracket = findFirstClosingBracket(newArray) + 1;
-    let numberOfBracketsBetween = indexOfSecondNeutralBracket - indexOfFirstNeutralBracket;
-    if (numberOfBracketsBetween % 2 == 0 ) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    return true;
-  }
+  return (bracket === neutralBracket)? true : false;
 }
 
-function findPairOfBrackets(array) {
-  let indexOfFirstClosingBracket = findFirstClosingBracket(array);
-  if (array[indexOfFirstClosingBracket] === '|') {
-    let result3 = checkForSecondNeutralBracket(array, indexOfFirstClosingBracket);
-    return (result3)? indexOfFirstClosingBracket : false;
-  } else if (indexOfFirstClosingBracket) {
-    let result1 = checkIfBracketIsOpening(array[indexOfFirstClosingBracket - 1]);
-    let result2 = checkIfBracketsAreMatch(array, indexOfFirstClosingBracket);
-    if (result1 && result2) {
-      return indexOfFirstClosingBracket;
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
+function checkForOpeningPair(bracket) {
+  let openingBrackets = ['(', '[', '{', '|'];
+  let closingBrackets = [')', ']', '}', '|'];  
+  let indexOfClosingBracket = closingBrackets.indexOf(bracket);
+  let openingPair =  openingBrackets[indexOfClosingBracket];
+  return openingPair;
 }
